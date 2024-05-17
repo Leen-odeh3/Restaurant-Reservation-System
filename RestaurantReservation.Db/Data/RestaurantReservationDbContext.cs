b49cc657a61233eec9235a71c2d23fc7a86c505d
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-05-17 21:37:02 +0300

    Add ConnectionString & config connection db

--- RestaurantReservation.Db/Data/RestaurantReservationDbContext.cs
+++ RestaurantReservation.Db/Data/RestaurantReservationDbContext.cs
@@ -1,9 +1,20 @@
-﻿
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace RestaurantReservation.Db.Data
 {
     public class RestaurantReservationDbContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var config = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            }
+        }
     }
 }
