3a816c691e02e899186215ee654a488d4cf79ac6
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-05-18 16:04:58 +0300

    GetReservationsByCustomer(CustomerId)

--- RestaurantReservation.Db/Repositories/ReservationRepository.cs
+++ RestaurantReservation.Db/Repositories/ReservationRepository.cs
@@ -1,13 +1,34 @@
-﻿using RestaurantReservation.Db.Abstracts;
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Abstracts;
 using RestaurantReservation.Db.Data;
 using RestaurantReservation.Db.Entities;
+using RestaurantReservation.Db.Exceptions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace RestaurantReservation.Db.Repositories
 {
     public class ReservationRepository : Repository<Reservation>, IReservationRepository
     {
-        public ReservationRepository(RestaurantReservationDbContext context) : base(context)
+        private readonly ICustomerRepository _customerRepository;
+
+        public ReservationRepository(RestaurantReservationDbContext context, ICustomerRepository customerRepository) : base(context)
         {
+            _customerRepository = customerRepository;
+        }
+
+        public async Task<List<Reservation>> GetReservationsByCustomer(int customerId)
+        {
+            var customer = await _customerRepository.GetByIdAsync(customerId);
+            if (customer is null)
+            {
+               throw new NotFoundException<Customer>($"Customer with id {customerId} not found.");
+            }
+
+            return await _context.Reservations
+                .Where(r => r.CustomerId == customerId)
+                .ToListAsync();
         }
     }
 }
