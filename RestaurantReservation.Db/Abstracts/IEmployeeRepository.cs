ec2f074bee11c5b909eb08d93da14376a4cdeefb
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-05-19 12:09:33 +0300

    add Task<List<Employee>>

--- RestaurantReservation.Db/Abstracts/IEmployeeRepository.cs
+++ RestaurantReservation.Db/Abstracts/IEmployeeRepository.cs
@@ -1,10 +1,11 @@
 ﻿using RestaurantReservation.Db.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace RestaurantReservation.Db.Abstracts
 {
     public interface IEmployeeRepository : IRepository<Employee>
     {
-
+       Task<List<Employee>> ListManagers();
     }
-
 }
