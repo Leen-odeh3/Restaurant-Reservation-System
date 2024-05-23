a2ba2ca018aabff58a74b0b7a4fb65678a04feaa
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-05-23 22:31:23 +0300

    test case ConnectionDB passed

---
+++ RestaurantReservation.Db.Test/DbConnectionTest/DbConnectionTest.cs
@@ -0,0 +1,22 @@
+﻿
+using RestaurantReservation.Db.Data;
+using RestaurantReservation.Db.Test.Helper;
+
+namespace RestaurantReservation.Db.Test.DbConnectionTest;
+public class DbConnectionTest
+{
+    [Fact]
+    public void check_Connection()
+    {
+        RestaurantReservationDbContext context = new RestaurantReservationDbContext(DatabaseConnectionTest.GetConnection());
+        
+        if(context is not null) { 
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+        }
+        Assert.NotNull(context);
+        Assert.NotEmpty(context.Reservations);
+    }
+}
---
+++ RestaurantReservation.Db.Test/Helper/DatabaseConnectionTest.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Data;
+
+namespace RestaurantReservation.Db.Test.Helper;
+
+public class DatabaseConnectionTest
+{
+    public static DbContextOptions<RestaurantReservationDbContext> GetConnection()
+    {
+        DbContextOptions<RestaurantReservationDbContext> _dbOptions;
+
+        var builder = new DbContextOptionsBuilder<RestaurantReservationDbContext>();
+        var conn = new SqliteConnection("DataSource=:memory:");
+
+        conn.Open();
+        _dbOptions= builder.UseSqlite(conn).Options;
+
+        return _dbOptions;
+    }
+}
