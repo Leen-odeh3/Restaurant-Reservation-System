793afc53d651ab8454ac1f25ec72199e94454ede
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-05-24 00:00:23 +0300

    Update testcase connection db

--- RestaurantReservation.Db.Test/Helper/DatabaseConnectionTest.cs
+++ RestaurantReservation.Db.Test/Helper/DatabaseConnectionTest.cs
@@ -1,6 +1,4 @@
-﻿
-using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using RestaurantReservation.Db.Data;
 
 namespace RestaurantReservation.Db.Test.Helper;
@@ -9,14 +7,9 @@ public class DatabaseConnectionTest
 {
     public static DbContextOptions<RestaurantReservationDbContext> GetConnection()
     {
-        DbContextOptions<RestaurantReservationDbContext> _dbOptions;
-
         var builder = new DbContextOptionsBuilder<RestaurantReservationDbContext>();
-        var conn = new SqliteConnection("DataSource=:memory:");
-
-        conn.Open();
-        _dbOptions= builder.UseSqlite(conn).Options;
-
-        return _dbOptions;
+        builder.UseSqlServer("Server=.;Database=RestaurantReservationCore;Trusted_Connection=True;MultipleActiveResultSets=true");
+        return builder.Options;
     }
+
 }
--- RestaurantReservation.Db.Test/RepositoriesTest/EmployeeRepositoryTest.cs
+++ RestaurantReservation.Db.Test/RepositoriesTest/EmployeeRepositoryTest.cs
@@ -1,8 +1,8 @@
-﻿using RestaurantReservation.Db.Entities;
-using RestaurantReservation.Db.Repositories;
-using RestaurantReservation.Db.Enums;
+﻿using RestaurantReservation.Db.Repositories;
 using RestaurantReservation.Db.Data;
 using RestaurantReservation.Db.Test.Helper;
+using RestaurantReservation.Db.Entities;
+using RestaurantReservation.Db.Enums;
 using Xunit;
 
 namespace RestaurantReservation.Db.Test.RepositoriesTest
@@ -11,6 +11,7 @@ namespace RestaurantReservation.Db.Test.RepositoriesTest
     {
         private readonly RestaurantReservationDbContext _options;
         private EmployeeRepository _repo;
+
         public EmployeeRepositoryTest()
         {
             _options = new RestaurantReservationDbContext(DatabaseConnectionTest.GetConnection());
@@ -20,14 +21,25 @@ namespace RestaurantReservation.Db.Test.RepositoriesTest
         [Fact]
         public async Task GetAll_ReturnAllEmployee()
         {
-            // Act
-            var employees = await _repo.GetAllAsync();
+            LoadEmployeeData(_options); 
 
-            // Assert
-            Assert.NotNull(employees);
-            Assert.Equal(5, employees.Count);
+            var res = await _repo.GetAllAsync(); 
+
+            Assert.NotEmpty(res);
+            Assert.Equal(5, res.Count());
         }
 
-       
+        private void LoadEmployeeData(RestaurantReservationDbContext option)
+        {
+            option.Employees.AddRange(
+                 new Employee { Id = 1, FirstName = "Ella", LastName = "Fitzgerald", Position = EmployeePosition.Manager, RestaurantId = 1 },
+                 new Employee { Id = 2, FirstName = "Louis", LastName = "Armstrong", Position = EmployeePosition.Chef, RestaurantId = 1 },
+                 new Employee { Id = 3, FirstName = "Billie", LastName = "Holiday", Position = EmployeePosition.Waiter, RestaurantId = 1 },
+                 new Employee { Id = 4, FirstName = "Charlie", LastName = "Parker", Position = EmployeePosition.Waiter, RestaurantId = 2 },
+                 new Employee { Id = 5, FirstName = "Dizzy", LastName = "Gillespie", Position = EmployeePosition.Cashier, RestaurantId = 2 }
+            );
+
+            _options.SaveChanges(); 
+        }
     }
 }
