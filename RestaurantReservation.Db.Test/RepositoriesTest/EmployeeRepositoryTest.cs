fb1198d7c45bd4e6a3e6c3d172160382a74fb069
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-05-24 00:46:55 +0300

    Refactor EmployeeRepo TestCases

--- RestaurantReservation.Db.Test/RepositoriesTest/EmployeeRepositoryTest.cs
+++ RestaurantReservation.Db.Test/RepositoriesTest/EmployeeRepositoryTest.cs
@@ -1,45 +1,103 @@
 ﻿using RestaurantReservation.Db.Repositories;
 using RestaurantReservation.Db.Data;
-using RestaurantReservation.Db.Test.Helper;
 using RestaurantReservation.Db.Entities;
 using RestaurantReservation.Db.Enums;
-using Xunit;
+using Microsoft.EntityFrameworkCore;
 
 namespace RestaurantReservation.Db.Test.RepositoriesTest
 {
     public class EmployeeRepositoryTest
     {
-        private readonly RestaurantReservationDbContext _options;
-        private EmployeeRepository _repo;
+        private readonly DbContextOptions<RestaurantReservationDbContext> _options;
 
         public EmployeeRepositoryTest()
         {
-            _options = new RestaurantReservationDbContext(DatabaseConnectionTest.GetConnection());
-            _repo = new EmployeeRepository(_options);
+            _options = new DbContextOptionsBuilder<RestaurantReservationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+        }
+
+        [Collection("Database Collection")]
+        public class DatabaseCollection : ICollectionFixture<DatabaseFixture>
+        {
         }
 
         [Fact]
-        public async Task GetAll_ReturnAllEmployee()
+        public async Task GetAll_ReturnAllEmployees()
         {
-            LoadEmployeeData(_options); 
+            // Arrange
+            using (var context = new RestaurantReservationDbContext(_options))
+            {
+                var repository = new EmployeeRepository(context);
+                await LoadEmployeeData(context);
 
-            var res = await _repo.GetAllAsync(); 
+                // Act
+                var employees = await repository.GetAllAsync();
 
-            Assert.NotEmpty(res);
-            Assert.Equal(5, res.Count());
+                // Assert
+                Assert.NotEmpty(employees);
+                Assert.Equal(5, employees.Count);
+            }
         }
 
-        private void LoadEmployeeData(RestaurantReservationDbContext option)
+        [Fact]
+        public async Task GetListManagers_FromEmployee_ReturnsListOfManagers()
         {
-            option.Employees.AddRange(
-                 new Employee { Id = 1, FirstName = "Ella", LastName = "Fitzgerald", Position = EmployeePosition.Manager, RestaurantId = 1 },
-                 new Employee { Id = 2, FirstName = "Louis", LastName = "Armstrong", Position = EmployeePosition.Chef, RestaurantId = 1 },
-                 new Employee { Id = 3, FirstName = "Billie", LastName = "Holiday", Position = EmployeePosition.Waiter, RestaurantId = 1 },
-                 new Employee { Id = 4, FirstName = "Charlie", LastName = "Parker", Position = EmployeePosition.Waiter, RestaurantId = 2 },
-                 new Employee { Id = 5, FirstName = "Dizzy", LastName = "Gillespie", Position = EmployeePosition.Cashier, RestaurantId = 2 }
-            );
+            // Arrange
+            using (var context = new RestaurantReservationDbContext(_options))
+            {
+                var repository = new EmployeeRepository(context);
+                await LoadEmployeeData(context); // Using the same data
 
-            _options.SaveChanges(); 
+                // Act
+                var managers = await repository.ListManagers();
+
+                // Assert
+                Assert.NotNull(managers);
+                Assert.NotEmpty(managers);
+                Assert.Equal(4, managers.Count);
+                Assert.All(managers, m => Assert.Equal(EmployeePosition.Manager, m.Position));
+            }
+        }
+
+        private async Task LoadEmployeeData(RestaurantReservationDbContext context)
+        {
+            var employees = new[]
+            {
+                new Employee { FirstName = "Ella", LastName = "Fitzgerald", Position = EmployeePosition.Manager, RestaurantId = 1 },
+                new Employee { FirstName = "Louis", LastName = "Armstrong", Position = EmployeePosition.Chef, RestaurantId = 1 },
+                new Employee { FirstName = "Billie", LastName = "Holiday", Position = EmployeePosition.Waiter, RestaurantId = 1 },
+                new Employee { FirstName = "Charlie", LastName = "Parker", Position = EmployeePosition.Manager, RestaurantId = 2 },
+                new Employee { FirstName = "Dizzy", LastName = "Gillespie", Position = EmployeePosition.Cashier, RestaurantId = 2 }
+            };
+
+            await context.AddRangeAsync(employees);
+            await context.SaveChangesAsync();
+        }
+    }
+
+    public class DatabaseFixture : IDisposable
+    {
+        public DatabaseFixture()
+        {
+            Options = new DbContextOptionsBuilder<RestaurantReservationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            using (var context = new RestaurantReservationDbContext(Options))
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+
+        public DbContextOptions<RestaurantReservationDbContext> Options { get; }
+
+        public void Dispose()
+        {
+            using (var context = new RestaurantReservationDbContext(Options))
+            {
+                context.Database.EnsureDeleted();
+            }
         }
     }
 }
